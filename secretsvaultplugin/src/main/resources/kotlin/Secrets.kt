09076d3e7ce27427c0a73b544eac6f1b9a75211b
Utku Kagitci <utku.kagitci@commencis.com>
2023-08-17 13:02:49 +0300

    Update native library name according to source set if needed

--- secretsvaultplugin/src/main/resources/kotlin/Secrets.kt
+++ secretsvaultplugin/src/main/resources/kotlin/Secrets.kt
@@ -4,7 +4,7 @@ class SECRETS_CLASS_NAME {
 EXTERNAL_METHODS_PLACEHOLDER
     companion object {
         init {
-            System.loadLibrary("secrets")
+            System.loadLibrary("NATIVE_FILE_NAME_PLACEHOLDER")
         }
     }
 }
